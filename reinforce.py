87209df8124409713698a883484a0cb7d553c268
Sergio <sergiogcharles@gmail.com>
2021-11-05 02:03:17 -0700

    Implements pupper reinforce run policy

--- reinforce.py
+++ reinforce.py
@@ -34,8 +34,7 @@ class Policy(nn.Module):
     def __init__(self):
         super(Policy, self).__init__()
         self.affine1 = nn.Linear(14, 128)
-        self.dropout = nn.Dropout(p=0.6)
-        # self.affine2 = nn.Linear(128, 16)
+        self.dropout = nn.Dropout(p=0.1)
         self.affine2 = nn.Linear(128, 32)
 
         self.saved_log_probs = []
@@ -46,7 +45,7 @@ class Policy(nn.Module):
         x = self.dropout(x)
         x = F.relu(x)
         action_scores = self.affine2(x)
-        # return F.softmax(action_scores, dim=1)
+        action_scores = F.tanh(action_scores)
         return action_scores
 
 policy = Policy()
@@ -57,19 +56,11 @@ eps = np.finfo(np.float32).eps.item()
 def select_action(state):
     state = torch.from_numpy(state).float().unsqueeze(0)
     normal_params = policy(state).reshape((2, 16))
-    mu, sigma = normal_params[0, :], torch.diag(torch.abs(normal_params[1, :]))
+    mu, sigma = normal_params[0], torch.diag(torch.abs(normal_params[1]))
     dist = MultivariateNormal(mu, sigma)
     action = dist.sample()
     policy.saved_log_probs.append(dist.log_prob(action))
     return action.detach().numpy()
-    # state = torch.from_numpy(state).float().unsqueeze(0)
-    # probs = policy(state).squeeze(0)
-    # dist = Categorical(probs)
-    # action = dist.sample()
-    # policy.saved_log_probs.append(dist.log_prob(action))
-    # print(action)
-    # return action.detach().numpy()
-
 
 def finish_episode():
     R = 0
@@ -81,7 +72,7 @@ def finish_episode():
     returns = torch.tensor(returns)
     returns = (returns - returns.mean()) / (returns.std() + eps)
     for log_prob, R in zip(policy.saved_log_probs, returns):
-        print(f'R {R}, log_prob {log_prob}')
+        # print(f'R {R}, log_prob {log_prob}')
         policy_loss.append(-log_prob.reshape(-1) * R)
     optimizer.zero_grad()
     policy_loss = torch.cat(policy_loss).sum()
@@ -110,6 +101,8 @@ def main():
         if i_episode % args.log_interval == 0:
             print('Episode {}\tLast reward: {:.2f}\tAverage reward: {:.2f}'.format(
                   i_episode, ep_reward, running_reward))
+            # save policy after each log interval
+            torch.save(policy.state_dict(), 'puppersim/puppersim/model.pt')
         # if running_reward > env.spec.reward_threshold:
         #     print("Solved! Running reward is now {} and "
         #           "the last episode runs to {} time steps!".format(running_reward, t))
