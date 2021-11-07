fa122d8308f5f6b0fb37a2b04f173de90691ff7e
Sergio <sergiogcharles@gmail.com>
2021-11-06 17:49:38 -0700

    Implements continuous reinforce policy gradient algorithm

--- reinforce.py
+++ reinforce.py
@@ -11,6 +11,7 @@ import torch.optim as optim
 from torch.distributions import Categorical
 from puppersim.pupper_gym_env import PupperGymEnv
 from torch.distributions import MultivariateNormal
+num_samples_per_training_step = 50
 
 parser = argparse.ArgumentParser(description='PyTorch REINFORCE example')
 parser.add_argument('--gamma', type=float, default=0.99, metavar='G',
@@ -19,7 +20,7 @@ parser.add_argument('--seed', type=int, default=543, metavar='N',
                     help='random seed (default: 543)')
 parser.add_argument('--render', action='store_true',
                     help='render the environment')
-parser.add_argument('--log-interval', type=int, default=10, metavar='N',
+parser.add_argument('--log-interval', type=int, default=1, metavar='N',
                     help='interval between training status logs (default: 10)')
 args = parser.parse_args()
 
@@ -35,71 +36,114 @@ class Policy(nn.Module):
         super(Policy, self).__init__()
         self.affine1 = nn.Linear(14, 128)
         self.dropout = nn.Dropout(p=0.1)
-        self.affine2 = nn.Linear(128, 32)
+        self.affine2 = nn.Linear(128, 16)
+        self.affine2_ = nn.Linear(128, 16)
 
-        self.saved_log_probs = []
+        self.log_probs = []
         self.rewards = []
 
     def forward(self, x):
         x = self.affine1(x)
         x = self.dropout(x)
         x = F.relu(x)
-        action_scores = self.affine2(x)
-        action_scores = F.tanh(action_scores)
-        return action_scores
+        # action_scores = self.affine2(x)
+        #action_scores = F.tanh(action_scores)
+
+        mu = self.affine2(x)
+        sigma_sq = self.affine2_(x)
+
+        return mu, sigma_sq
 
 policy = Policy()
-optimizer = optim.Adam(policy.parameters(), lr=1e-2)
+optimizer = optim.Adam(policy.parameters(), lr=1e-3)
 eps = np.finfo(np.float32).eps.item()
 
 
 def select_action(state):
     state = torch.from_numpy(state).float().unsqueeze(0)
-    normal_params = policy(state).reshape((2, 16))
-    mu, sigma = normal_params[0], torch.diag(torch.abs(normal_params[1]))
-    dist = MultivariateNormal(mu, sigma)
+    mu, sigma_sq = policy(state)
+    mu = mu.reshape(-1)
+    sigma_sq = sigma_sq.reshape(-1)
+    # mu, sigma = torch.tanh(normal_params[0]), torch.diag(torch.sigmoid(normal_params[1]))
+    sigma_sq = F.softplus(sigma_sq)
+    dist = MultivariateNormal(mu, torch.diag(sigma_sq))
     action = dist.sample()
-    policy.saved_log_probs.append(dist.log_prob(action))
-    return action.detach().numpy()
+    #policy.saved_log_probs.append(dist.log_prob(action))
+    return action.detach().numpy(), dist.log_prob(action)
 
 def finish_episode():
-    R = 0
     policy_loss = []
-    returns = []
-    for r in policy.rewards[::-1]:
-        R = r + args.gamma * R
-        returns.insert(0, R)
-    returns = torch.tensor(returns)
-    returns = (returns - returns.mean()) / (returns.std() + eps)
-    for log_prob, R in zip(policy.saved_log_probs, returns):
-        # print(f'R {R}, log_prob {log_prob}')
-        policy_loss.append(-log_prob.reshape(-1) * R)
+    for trajectory_rewards, trajectory_log_probs in zip(policy.rewards, policy.log_probs):
+        #discount our rewards
+        R = 0
+        returns_trajectory = []
+        for r in trajectory_rewards[::-1]:
+            R = r + args.gamma * R
+            returns_trajectory.insert(0, R)
+        returns_trajectory = torch.tensor(returns_trajectory)
+        returns_trajectory = (returns_trajectory - returns_trajectory.mean()) / (returns_trajectory.std() + eps)
+        #weight our actions for our policy gradient
+        for log_prob, R in zip(trajectory_log_probs, returns_trajectory):
+            policy_loss.append(-log_prob.reshape(-1) * R)
     optimizer.zero_grad()
-    policy_loss = torch.cat(policy_loss).sum()
+    policy_loss = torch.cat(policy_loss).sum() / num_samples_per_training_step
     policy_loss.backward()
     optimizer.step()
     del policy.rewards[:]
-    del policy.saved_log_probs[:]
+    del policy.log_probs[:]
+
+    # R = 0
+    # policy_loss = []
+    # returns = []
+    # for r in policy.rewards[::-1]:
+    #     R = r + args.gamma * R
+    #     returns.insert(0, R)
+    # returns = torch.tensor(returns)
+    # returns = (returns - returns.mean()) / (returns.std() + eps)
+    # for log_prob, R in zip(policy.saved_log_probs, returns):
+    #     # print(f'R {R}, log_prob {log_prob}')
+    #     policy_loss.append(-log_prob.reshape(-1) * R)
+    # optimizer.zero_grad()
+    # policy_loss = torch.cat(policy_loss).sum()
+    # policy_loss.backward()
+    # optimizer.step()
+    # del policy.rewards[:]
+    # del policy.saved_log_probs[:]
 
 
 def main():
     running_reward = 10
     for i_episode in count(1):
-        state, ep_reward = env.reset(), 0
-        for t in range(1, 10000):  # Don't infinite loop while learning
-            action = select_action(state)
-            state, reward, done, _ = env.step(action)
-            if args.render:
-                env.render()
-            policy.rewards.append(reward)
-            ep_reward += reward
-            if done:
-                break
 
-        running_reward = 0.05 * ep_reward + (1 - 0.05) * running_reward
+        for i in range(num_samples_per_training_step):
+            trajectory_reward = []
+            trajectory_log_probs = []
+            state, ep_reward = env.reset(), 0
+            for t in range(1, 10000):  # Don't infinite loop while learning
+                action, log_prob = select_action(state)
+                state, reward, done, _ = env.step(action)
+                if args.render:
+                    env.render()
+                #policy.rewards.append(reward)
+                trajectory_reward.append(reward)
+                trajectory_log_probs.append(log_prob)
+                ep_reward += reward
+                if done:
+                    break
+            #trajectory has been rolled out
+            policy.rewards.append(trajectory_reward) #appending a list of rewards
+            policy.log_probs.append(trajectory_log_probs) #appending a list of log_probs
+            #print(trajectory_reward)
+            print(f"trajectory {i} finished")
+
+
+        ep_reward = ep_reward / num_samples_per_training_step
+        running_reward = 0.05 * (ep_reward) + (1 - 0.05) * running_reward
+        print("step finished")
         finish_episode()
+
         if i_episode % args.log_interval == 0:
-            print('Episode {}\tLast reward: {:.2f}\tAverage reward: {:.2f}'.format(
+            print('Episode {}\tLast reward: {:.10f}\tAverage reward: {:.2f}'.format(
                   i_episode, ep_reward, running_reward))
             # save policy after each log interval
             torch.save(policy.state_dict(), 'puppersim/puppersim/model.pt')
---
+++ reinforce_continuous.py
@@ -0,0 +1,156 @@
+import sys
+import math
+
+import torch
+import torch.autograd as autograd
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import torch.nn.utils as utils
+import torchvision.transforms as T
+from torch.autograd import Variable
+
+import argparse, math, os
+import numpy as np
+import gym
+from gym import wrappers
+
+from puppersim.pupper_gym_env import PupperGymEnv
+
+pi = Variable(torch.FloatTensor([math.pi]))
+
+def normal(x, mu, sigma_sq):
+    a = (-1*(Variable(x)-mu).pow(2)/(2*sigma_sq)).exp()
+    b = 1/(2*sigma_sq*pi.expand_as(sigma_sq)).sqrt()
+    return a*b
+
+
+class Policy(nn.Module):
+    def __init__(self, hidden_size=128, num_inputs=14, action_space=16):
+        super(Policy, self).__init__()
+        self.action_space = action_space
+        num_outputs = action_space.shape[0]
+
+        self.linear1 = nn.Linear(num_inputs, hidden_size)
+        self.hidden_linear1 = nn.Linear(hidden_size, hidden_size)
+        self.hidden_linear2 = nn.Linear(hidden_size, hidden_size)
+        self.hidden_linear3 = nn.Linear(hidden_size, hidden_size)
+
+        self.linear2 = nn.Linear(hidden_size, num_outputs)
+        self.linear2_ = nn.Linear(hidden_size, num_outputs)
+
+    def forward(self, inputs):
+        x = inputs
+        x = F.relu(self.linear1(x))
+        x = F.relu(self.hidden_linear1(x))
+        x = F.relu(self.hidden_linear2(x))
+        x = F.relu(self.hidden_linear3(x))
+        mu = self.linear2(x)
+        sigma_sq = self.linear2_(x)
+
+        return mu, sigma_sq
+
+
+class REINFORCE:
+    def __init__(self, hidden_size=128, num_inputs=14, action_space=16):
+        self.action_space = action_space
+        self.model = Policy(hidden_size, num_inputs, action_space)
+        self.optimizer = optim.Adam(self.model.parameters(), lr=1e-2)
+        self.model.train()
+
+    def select_action(self, state):
+        mu, sigma_sq = self.model(Variable(state))
+        mu = mu.clamp(-pi/8, pi/8)
+        sigma_sq = F.softplus(sigma_sq + 1e-5)
+        # sigma_sq = sigma_sq.clamp(1e-5, 0.1)
+
+        eps = torch.randn(mu.size())
+        # calculate the probability
+        action = (mu + sigma_sq.sqrt()*Variable(eps)).data
+        prob = normal(action, mu, sigma_sq)
+        entropy = -0.5 * ((sigma_sq + 2 * pi.expand_as(sigma_sq)).log() + 1)
+
+        log_prob = prob.log()
+        return action, log_prob, entropy
+
+    def update_parameters(self, rewards, log_probs, entropies, gamma):
+        R = torch.zeros(1, 1)
+        loss = 0
+        for i in reversed(range(len(rewards))):
+            R = gamma * R + rewards[i]
+            loss = loss - (log_probs[i]*(Variable(R).expand_as(log_probs[i]))).sum() - (0.0001*entropies[i]).sum()
+        loss = loss / len(rewards)
+		
+        self.optimizer.zero_grad()
+        loss.backward()
+        utils.clip_grad_norm(self.model.parameters(), 40)
+        self.optimizer.step()
+
+
+parser = argparse.ArgumentParser(description='PyTorch REINFORCE example')
+parser.add_argument('--env_name', type=str, default='CartPole-v0')
+parser.add_argument('--gamma', type=float, default=0.99, metavar='G',
+                    help='discount factor for reward (default: 0.99)')
+parser.add_argument('--exploration_end', type=int, default=100, metavar='N',
+                    help='number of episodes with noise (default: 100)')
+parser.add_argument('--seed', type=int, default=123, metavar='N',
+                    help='random seed (default: 123)')
+parser.add_argument('--num_steps', type=int, default=1000, metavar='N',
+                    help='max episode length (default: 1000)')
+parser.add_argument('--num_episodes', type=int, default=10000, metavar='N',
+                    help='number of episodes (default: 10000)')
+parser.add_argument('--hidden_size', type=int, default=128, metavar='N',
+                    help='number of episodes (default: 128)')
+parser.add_argument('--render', action='store_true',
+                    help='render the environment')
+parser.add_argument('--ckpt_freq', type=int, default=100, 
+		    help='model saving frequency')
+parser.add_argument('--display', type=bool, default=False,
+                    help='display or not')
+args = parser.parse_args()
+
+# env_name = args.env_name
+# env = gym.make(env_name)
+env = PupperGymEnv()
+# if type(env.action_space) != gym.spaces.discrete.Discrete:
+#     from reinforce_continuous import REINFORCE
+#     env = NormalizedActions(gym.make(env_name))
+# else:
+#     from reinforce_discrete import REINFORCE
+
+# if args.display:
+#     env = wrappers.Monitor(env, '/tmp/{}-experiment'.format(env_name), force=True)
+
+# env.seed(args.seed)
+# torch.manual_seed(args.seed)
+# np.random.seed(args.seed)
+
+agent = REINFORCE(args.hidden_size, env.observation_space.shape[0], env.action_space)
+
+for i_episode in range(args.num_episodes):
+    state = torch.Tensor([env.reset()])
+    entropies = []
+    log_probs = []
+    rewards = []
+    for t in range(args.num_steps):
+        action, log_prob, entropy = agent.select_action(state)
+        action = action.cpu()
+
+        next_state, reward, done, _ = env.step(action.numpy()[0])
+
+        entropies.append(entropy)
+        log_probs.append(log_prob)
+        rewards.append(reward)
+        state = torch.Tensor([next_state])
+
+        if done:
+            # print(f'Episode {i_episode} Time-step {t} Reward {reward}')
+            break
+
+    agent.update_parameters(rewards, log_probs, entropies, args.gamma)
+
+    if i_episode%args.ckpt_freq == 0:
+        torch.save(agent.model.state_dict(), 'puppersim/puppersim/model.pt')
+        print("Episode: {}, reward: {}".format(i_episode, np.sum(rewards)))
+	
+env.close()
